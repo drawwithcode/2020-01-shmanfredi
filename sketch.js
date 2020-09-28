5e6db4eea9e6e7d82b12c541604a16d313b0b5ec
Sharon Manfredi <sharon.manfredi@mail.polimi.it>
2020-09-28 12:22:10 +0200

    upload

--- sketch.js
+++ sketch.js
@@ -5,8 +5,29 @@ function preload(){
 function setup() {
   createCanvas(windowWidth,windowHeight)
   // put setup code here
+  background('black');
+  rectMode(CENTER);
+
 }
 
 function draw() {
   // put drawing code here
+  push();
+  noFill();
+  stroke(lerpColor(color('Cyan'), color('Crimson'), frameCount / 540));
+  strokeWeight(0.01);
+
+  translate (width / 2, height / 2);
+  scale(frameCount/120);
+
+  rotate(frameCount/120);
+  rect(0, 0, 100, 100);
+
+
+  if (frameCount == 1080) {
+    noLoop();
+  }
+  pop();
+
+
 }
